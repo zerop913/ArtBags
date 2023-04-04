cbe541b61195f947e9266ed68285a12168ae8a90
zerop913 <128187844+zerop913@users.noreply.github.com>
2023-04-04 19:01:08 +0300

    config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      lg: { max: "1299.99px" },
+      md: { max: "1039.99px" },
+      sm: { max: "767.99px" },
+      xs: { max: "424.99px" },
+    },
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+      backgroundColor: {
+        heroColor: "#08162B",
+      },
+      backgroundImage: {
+        heroGradient:
+          "linear-gradient(108.27deg, rgba(255, 255, 255, 0.5) 3.59%, rgba(255, 255, 255, 0.2) 98.72%)",
+      },
+      colors: {
+        white: "#FFFFFF",
+        orange: "#F29494",
+      },
+    },
+  },
+  plugins: [],
+};
