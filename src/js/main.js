776465300e07126ff875259eb8c9c6f58214e061
zerop913 <128187844+zerop913@users.noreply.github.com>
2023-04-10 16:14:27 +0300

    scroll

--- src/js/main.js
+++ src/js/main.js
@@ -24,18 +24,6 @@ toggleMenu();
 var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 var rootElement = document.documentElement;
 
-function handleScroll() {
-  // Do something on scroll
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-  if (rootElement.scrollTop / scrollTotal > 0.8) {
-    // Show button
-    scrollToTopBtn.classList.add("showBtn");
-  } else {
-    // Hide button
-    scrollToTopBtn.classList.remove("showBtn");
-  }
-}
-
 function scrollToTop() {
   // Scroll to top logic
   rootElement.scrollTo({
@@ -44,4 +32,3 @@ function scrollToTop() {
   });
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
-document.addEventListener("scroll", handleScroll);
