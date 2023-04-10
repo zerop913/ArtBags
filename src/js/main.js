a92603e184825d6cc5d8ce91df1ef5d7837637fb
zerop913 <128187844+zerop913@users.noreply.github.com>
2023-04-10 16:17:29 +0300

    not

--- src/js/main.js
+++ src/js/main.js
@@ -20,15 +20,3 @@ function toggleMenu() {
   });
 }
 toggleMenu();
-
-var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-var rootElement = document.documentElement;
-
-function scrollToTop() {
-  // Scroll to top logic
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-}
-scrollToTopBtn.addEventListener("click", scrollToTop);
